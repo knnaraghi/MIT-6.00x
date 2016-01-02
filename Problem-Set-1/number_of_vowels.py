44bd3d75ee03ff2996be83683313f8030b89ecfe
knnaraghi <knnaraghi@gmail.com>
2016-01-02 00:21:04 -0500

    Create number_of_vowels.py

---
+++ Problem-Set-1/number_of_vowels.py
@@ -0,0 +1,6 @@
+s = 'coolio'
+vowels = 0
+for x in s:
+    if x == 'a' or x == 'e' or x == 'i' or x == 'o' or x== 'u' or x == 'A' or x == 'E' or x == 'I' or x == 'O' or x == 'U':
+        vowels += 1
+print ("Number of Vowels:" + str(vowels))
